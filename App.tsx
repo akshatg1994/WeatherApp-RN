d54a0b2570e559b7a5d39e87bd36f68d905fe460
Akshat <akshat.g1994@gmail.com>
2025-09-21 14:52:44 +0530

    Weather App initial commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={{
+        flex: 1,
+        backgroundColor: 'red'
+      }}>
+
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
